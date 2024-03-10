4abdb1ebddf158e780b880715d3ad010775eea05
nikhil pandey <thenikhilpandey11@gmail.com>
2024-03-10 21:10:40 +0530

    Ui completed

---
+++ frontend/src/App.jsx
@@ -0,0 +1,25 @@
+import { Routes, Route } from "react-router-dom";
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
+import TransactionPage from './pages/TransactionPage';
+import NotFoundPage from './pages/NotFoundPage';
+import Header from './components/ui/Header';
+
+function App() {
+   const authUser=true;
+  return (
+    <>
+       {authUser && <Header/> }
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/transaction/:id" element={<TransactionPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
